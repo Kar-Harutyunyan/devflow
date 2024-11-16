2023e92cf2559181e5ab00614ac5e8c5b573a417
Karen <karenharutyunyan743@gmail.com>
2024-11-17 00:01:58 +0400

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and Github providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import {Metadata} from "next";
 import {ThemeProvider} from "next-themes";
 import Navbar from "@/components/navigation/navbar";
+import {Toaster} from "@/components/ui/toaster";
+import {SessionProvider} from "next-auth/react";
+import {auth} from "@/auth";
 
 const inter = localFont({
-  src: "./fonts/InterVF.ttf",
-  variable: "--font-inter",
-  weight: "100 200 300 400 500 700 800 900",
+    src: "./fonts/InterVF.ttf",
+    variable: "--font-inter",
+    weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
@@ -27,22 +30,25 @@ export const metadata: Metadata = {
     },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+export default async function RootLayout({
+                                             children,
+                                         }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable}  antialiased`}
-      >
-      <ThemeProvider attribute='class' defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Navbar />
-          {children}
-      </ThemeProvider>
-      </body>
-    </html>
-  );
+    const session = await auth();
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <SessionProvider session={session}>
+            <body
+                className={`${inter.className} ${spaceGrotesk.variable}  antialiased`}
+            >
+            <ThemeProvider attribute='class' defaultTheme="system" enableSystem disableTransitionOnChange>
+                {children}
+            </ThemeProvider>
+            <Toaster/>
+            </body>
+        </SessionProvider>
+        </html>
+    );
 
 }
