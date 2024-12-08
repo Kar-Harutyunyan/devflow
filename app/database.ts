32139f09a63de56e1019e973af25fc0d55d5132d
Karen <karenharutyunyan743@gmail.com>
2024-12-08 22:27:45 +0400

    Implement Create & Read User Routes

---
+++ app/database.ts
@@ -0,0 +1,21 @@
+const tickets = [
+    {
+        id:1,
+        name: 'Fix next.js API routes bug',
+        status:"open",
+        type:'bug'
+    },
+    {
+        id:2,
+        name: 'Add a new feature to app',
+        status:"open",
+        type:'feature'
+    },
+    {
+        id:3,
+        name: 'Refactor the app',
+        status:"open",
+        type:'refactor'
+    }]
+
+export default tickets
